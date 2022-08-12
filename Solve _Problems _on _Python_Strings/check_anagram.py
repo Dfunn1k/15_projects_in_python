c3e7d993c022355ade4217c600f05c848e50d63e
BigDany1792 <dany1792dota@gmail.com>
2022-08-11 21:52:18 -0500

    ci: added function that check anagram

---
+++ Solve _Problems _on _Python_Strings/check_anagram.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def check_anagram(word1, word2):
+    """Function that check if a word is
+    anagram.
+
+    Args:
+        word1 (str): word to check.
+        word2 (str): word to check.
+    
+    Attribute:
+        check_list (list): each letter of
+        the word will be added here.
+
+    Returns:
+        True if is anagram else return False.
+    """
+    check_list = [x for x in word2]
+    for char in word1:
+        if char in check_list:
+            check_list.remove(char)
+    if len(check_list) == 0:
+        return True
+    return False
